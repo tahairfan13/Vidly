a1731d260ceea275006cdf791ddf97af162086f1
Taha Ali <tahairfan1993@gmail.com>
2019-02-26 01:20:08 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json()); // To get data in JSON form
+
+const genres = [
+    {id: 1, name: "Terminator"},
+    {id: 2, name: "Warrior"}
+]
+
+
+app.get('/', (req, res) => {
+    res.send("I am the index page");
+})
+
+//Index
+app.get('/api/genres', (req, res) => {
+   res.send(genres); 
+})
+
+//Create
+app.post('/api/genres', (req, res)=>{
+    const { error } = validateMovie(req.body);
+    if (error)  {
+      res.status(400).send({"error": error.details[0].message});
+    }
+    else {
+        const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+        }
+        genres.push(genre);
+        res.send(genres);
+    }
+})
+
+//Show
+app.get('/api/genres/:id', (req, res)=> {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with given ID does not exist!");
+    res.send(genre);
+})
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with given ID does not exist!");
+
+    const { error } = validateMovie(req.body);
+    if (error)  {
+      res.status(400).send({"error": error.details[0].message});
+    }
+    else {
+        genre.name = req.body.name;
+        res.send(genre);
+    }
+})
+
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with given ID does not exist!");
+});
+
+const validateMovie = (course) => {
+    const schema = {
+        name: Joi.string().min(4).max(8).required()
+    }
+    return Joi.validate(course, schema);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server is running"));
